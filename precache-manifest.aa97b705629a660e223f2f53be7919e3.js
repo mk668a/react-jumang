141988f37901f70a89e943872a2d7b2b503ce5ca
jumang4423 <catontheskate4423@gmail.com>
2020-09-24 01:50:23 +0900

    Updates

---
+++ precache-manifest.aa97b705629a660e223f2f53be7919e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cbe0eb90dbba78776bb18817587bb5",
+    "url": "/react-jumang/index.html"
+  },
+  {
+    "revision": "4770782b6406c88ade54",
+    "url": "/react-jumang/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ffd3cc220d7a515bae8",
+    "url": "/react-jumang/static/js/2.c1192bcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-jumang/static/js/2.c1192bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4770782b6406c88ade54",
+    "url": "/react-jumang/static/js/main.34847738.chunk.js"
+  },
+  {
+    "revision": "ea2a66ee35587d44c8c5",
+    "url": "/react-jumang/static/js/runtime-main.b6083c5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-jumang/static/media/logo.5d5d9eef.svg"
+  }
+]);
